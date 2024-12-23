7907fd205f83e685e6eab23441356a73d7533f0f
pujisubarkah <pujiatmo.subarkah@gmail.com>
2024-12-23 23:21:12 +0700

    push dari depok

--- src/components/editpegawai.tsx
+++ src/components/editpegawai.tsx
@@ -5,125 +5,203 @@ import { useRouter, useParams } from "next/navigation";
 import axios from "axios";
 
 interface Pegawai {
-    peg_nama: string;
-    peg_nip: string;
-    peg_gelar_depan: string | null; // Sesuaikan jika nilai ini bisa `null`
+  satuan_kerja_id?: string;
+  unit_kerja_id?: string;
+  peg_nip?: string;
+  peg_nip_lama?: string;
+  peg_nama?: string;
+  peg_gelar_depan?: string;
+  peg_gelar_belakang?: string;
+  peg_lahir_tempat?: string;
+  peg_tgl_lahir?: string;
+  peg_foto?: string;
+  peg_jenis_kelamin?: string;
+  peg_status_perkawinan?: string;
+  id_agama?: string;
+  peg_jenis_asn?: string;
+  peg_jenis_pns?: string;
+  status_tkd?: string;
+  peg_status_kepegawaian?: string;
+  peg_cpns_tmt?: string;
+  peg_pns_tmt?: string;
+  id_pend_awal?: string;
+  peg_pend_awal_th?: string;
+  id_pend_akhir?: string;
+  peg_pend_akhir_th?: string;
+  jabatan_id?: string;
+  peg_status_gaji?: string;
+  kedudukan_pegawai?: string;
+  gol_id_awal?: string;
+  peg_gol_tmt_awal?: string;
+  gol_id_akhir?: string;
+  peg_gol_tmt_akhir?: string;
+  peg_karpeg?: string;
+  peg_no_askes?: string;
+  peg_karsutri?: string;
+  peg_bapertarum?: string | null;
 }
 
 const EditPegawai: React.FC = () => {
-    const router = useRouter();
-    const params = useParams<{ pegid: string }>();
-    const pegid = params?.pegid || "";
-    const [pegawai, setPegawai] = useState<Pegawai | null>(null);
-    const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+  const { pegid } = useParams() as { pegid: string };
+  const [pegawai, setPegawai] = useState<Pegawai | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
-    useEffect(() => {
-        if (!pegid) {
-            console.error("ID tidak ditemukan. Mengarahkan pengguna ke halaman utama.");
-            setError("ID tidak ditemukan. Anda akan diarahkan ke halaman utama.");
-            setTimeout(() => {
-                router.push("/edit-pegawai");
-            }, 3000);
-            return;
-        }
-
-        console.log("Fetching data for pegid:", pegid);
-
-        axios
-            .get(`/api/pegawai_unit/${pegid}`)
-            .then((response) => {
-                console.log("Data fetched successfully:", response.data);
-
-                // Ekstrak hanya kolom yang diperlukan
-                const { peg_nama, peg_nip, peg_gelar_depan } = response.data;
-
-                setPegawai({
-                    peg_nama,
-                    peg_nip,
-                    peg_gelar_depan,
-                });
-            })
-            .catch((error) => {
-                console.error("Error fetching data:", error.response || error.message);
-                setError("Gagal memuat data pegawai. Silakan coba lagi.");
-            });
-    }, [pegid]);
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setPegawai((prevState) =>
-            prevState ? { ...prevState, [name]: value } : null
-        );
-    };
-
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        if (!pegawai) {
-            console.error("Tidak ada data untuk disimpan.");
-            setError("Tidak ada data yang dapat disimpan.");
-            return;
-        }
-
-        axios
-            .put(`/api/pegawai_unit/${pegid}`, pegawai)
-            .then((response) => {
-                console.log("Data updated successfully:", response.data);
-                router.push("/edit-pegawai");
-            })
-            .catch((error) => {
-                console.error("Error updating data:", error.response || error.message);
-                setError("Gagal memperbarui data. Silakan coba lagi.");
-            });
-    };
-
-    if (error) {
-        return (
-            <div>
-                <p>{error}</p>
-                <a href="/edit-pegawai">Kembali ke halaman utama</a>
-            </div>
-        );
+  useEffect(() => {
+    if (!pegid) {
+      setError("ID not found. Redirecting to the main page.");
+      router.push("/edit-pegawai");
+      return;
     }
 
+    axios
+      .get(`/api/pegawai_unit/${pegid}`)
+      .then((response) => {
+        setPegawai(response.data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setError("Failed to load employee data. Please try again.");
+        setLoading(false);
+      });
+  }, [pegid, router]);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setPegawai((prevState) =>
+      prevState ? { ...prevState, [name]: value } : null
+    );
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
     if (!pegawai) {
-        return <div>Loading...</div>;
+      setError("No data to save.");
+      return;
+    }
+
+    try {
+      await axios.put(`/api/pegawai_unit/${pegid}`, pegawai);
+      router.push("/edit-pegawai");
+    } catch {
+      setError("Failed to update data. Please try again.");
+    }
+  };
+
+  const renderFormField = (label: string, key: string, type: string) => {
+    if (key === "peg_status_perkawinan") {
+      // For peg_status_perkawinan, render radio buttons
+      const options = [
+        { label: "Kawin", value: "1" },
+        { label: "Belum Kawin", value: "2" },
+        { label: "Janda", value: "3" },
+        { label: "Duda", value: "4" },
+      ];
+
+      return (
+        <div key={key} className="mb-4">
+          <label
+            htmlFor={key}
+            className="block text-gray-700 text-sm font-bold col-span-1 text-right pr-4"
+          >
+            {label}:
+          </label>
+          <div className="flex gap-4 mt-2">
+            {options.map((option) => (
+              <div key={option.value} className="flex items-center">
+                <input
+                  type="radio"
+                  id={option.value}
+                  name={key}
+                  value={option.value}
+                  checked={pegawai?.[key as keyof Pegawai] === option.value}
+                  onChange={handleChange}
+                  className="mr-2"
+                />
+                <label htmlFor={option.value} className="text-gray-700">
+                  {option.label}
+                </label>
+              </div>
+            ))}
+          </div>
+        </div>
+      );
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="peg_nama">Nama:</label>
-                <input
-                    type="text"
-                    id="peg_nama"
-                    name="peg_nama"
-                    value={pegawai.peg_nama || ""} // Tampilkan string kosong jika data null
-                    onChange={handleChange}
-                />
-            </div>
-            <div>
-                <label htmlFor="peg_nip">NIP:</label>
-                <input
-                    type="text"
-                    id="peg_nip"
-                    name="peg_nip"
-                    value={pegawai.peg_nip || ""} // Tampilkan string kosong jika data null
-                    onChange={handleChange}
-                />
-            </div>
-            <div>
-                <label htmlFor="peg_gelar_depan">Gelar Depan:</label>
-                <input
-                    type="text"
-                    id="peg_gelar_depan"
-                    name="peg_gelar_depan"
-                    value={pegawai.peg_gelar_depan || ""} // Tampilkan string kosong jika data null
-                    onChange={handleChange}
-                />
-            </div>
-            <button type="submit">Submit</button>
-        </form>
+      <div key={key} className="mb-4 flex items-center">
+        <label
+          htmlFor={key}
+          className="block text-gray-700 text-sm font-bold mr-2 w-1/6"
+        >
+          {label}:
+        </label>
+        <input
+          id={key}
+          name={key}
+          type={type}
+          value={pegawai?.[key as keyof Pegawai] || ""}
+          onChange={handleChange}
+          placeholder={label}
+          className="shadow border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-gray-300"
+            />
+      </div>
     );
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div>
+        <p className="text-red-600">{error}</p>
+        <a
+          href="/edit-pegawai"
+          className="text-blue-600 underline hover:text-blue-800"
+        >
+          Return to main page
+        </a>
+      </div>
+    );
+  }
+
+  const formFields = [
+    { label: "Satuan Kerja", key: "satuan_kerja_id", type: "text" },
+    { label: "Unit Kerja", key: "unit_kerja_id", type: "text" },
+    { label: "NIP", key: "peg_nip", type: "text" },
+    { label: "Nama Lengkap", key: "peg_nama", type: "text" },
+    { label: "Tanggal Lahir", key: "peg_tgl_lahir", type: "date" },
+    { label: "Jenis Kelamin", key: "peg_jenis_kelamin", type: "text" },
+    { label: "Status Perkawinan", key: "peg_status_perkawinan", type: "text" },
+    { label: "Foto", key: "peg_foto", type: "text" },
+    { label: "agama", key: "id_agama", type: "text" },
+  ];
+
+  return (
+    <div className="m-2 w-full">
+      <form onSubmit={handleSubmit} className="w-full mx-auto">
+        <h1 className="text-center font-bold uppercase mb-6">Edit Pegawai</h1>
+        <div className="border p-4 rounded-lg">
+          {formFields.map((field) =>
+            renderFormField(field.label, field.key, field.type)
+          )}
+        </div>
+        <div className="flex items-center justify-center mt-4">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Simpan
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default EditPegawai;
--- src/pages/api/pegawai_unit/[id].ts
+++ src/pages/api/pegawai_unit/[id].ts
@@ -13,7 +13,40 @@ export default async function handler(req, res) {
       const { data, error } = await supabase
       .schema('siap_skpd')
         .from('spg_pegawai')
-        .select('peg_nama, peg_nip, peg_gelar_depan') // Kolom yang diambil
+        .select(`satuan_kerja_id, 
+                  unit_kerja_id,
+                   peg_nip,
+                   peg_nip_lama,
+                   peg_nama,
+                   peg_gelar_depan,
+                   peg_gelar_belakang,
+                   peg_lahir_tempat,
+                   peg_lahir_tanggal,
+                   peg_foto,
+                   peg_jenis_kelamin,
+                   peg_status_perkawinan,
+                   id_agama,
+                   peg_jenis_asn,
+                   peg_jenis_pns,
+                  status_tkd,
+                  peg_status_kepegawaian,
+                  peg_cpns_tmt,
+                  peg_pns_tmt,
+                  id_pend_awal,
+                  peg_pend_awal_th,
+                  id_pend_akhir,
+                  peg_pend_akhir_th,
+                  jabatan_id,
+                  peg_status_gaji,
+                  kedudukan_pegawai,
+                  gol_id_awal,
+                  peg_gol_awal_tmt,
+                  gol_id_akhir,
+                  peg_gol_akhir_tmt,
+                  peg_karpeg,
+                  peg_no_askes,
+                  peg_karsutri,
+                  peg_bapertarum`) // Kolom yang diambil
         .eq('peg_id', id);
 
       if (error) throw error;
@@ -28,7 +61,7 @@ export default async function handler(req, res) {
     }
   } else if (method === 'POST') {
     try {
-      const { peg_nama, peg_nip, peg_gelar_depan } = req.body;
+      const { peg_nama, peg_nip, peg_gelar_depan, satuan_kerja_id, unit_kerja_id, peg_nip_lama, peg_gelar_belakang, peg_lahir_tempat, peg_lahir_tanggal, peg_foto, peg_jenis_kelamin, peg_status_perkawinan, id_agama, peg_jenis_asn, peg_jenis_pns, status_tkd, peg_status_kepegawaian, peg_cpns_tmt, peg_pns_tmt, id_pend_awal, peg_pend_awal_th, id_pend_akhir, peg_pend_akhir_th, jabatan_id, peg_status_gaji, kedudukan_pegawai, gol_id_awal, peg_gol_awal_tmt, gol_id_akhir, peg_gol_akhir_tmt, peg_karpeg, peg_no_askes, peg_karsutri, peg_bapertarum } = req.body;
 
       if (!peg_nama || !peg_nip) {
         return res.status(400).json({ message: 'Nama dan NIP wajib diisi' });
@@ -37,7 +70,42 @@ export default async function handler(req, res) {
       const { data, error } = await supabase
       .schema('siap_skpd')
         .from('spg_pegawai')
-        .insert([{ peg_nama, peg_nip, peg_gelar_depan }]);
+        .insert([{
+          satuan_kerja_id,
+          unit_kerja_id,
+          peg_nip,
+          peg_nip_lama,
+          peg_nama,
+          peg_gelar_depan,
+          peg_gelar_belakang,
+          peg_lahir_tempat,
+          peg_lahir_tanggal,
+          peg_foto,
+          peg_jenis_kelamin,
+          peg_status_perkawinan,
+          id_agama,
+          peg_jenis_asn,
+          peg_jenis_pns,
+          status_tkd,
+          peg_status_kepegawaian,
+          peg_cpns_tmt,
+          peg_pns_tmt,
+          id_pend_awal,
+          peg_pend_awal_th,
+          id_pend_akhir,
+          peg_pend_akhir_th,
+          jabatan_id,
+          peg_status_gaji,
+          kedudukan_pegawai,
+          gol_id_awal,
+          peg_gol_awal_tmt,
+          gol_id_akhir,
+          peg_gol_akhir_tmt,
+          peg_karpeg,
+          peg_no_askes,
+          peg_karsutri,
+          peg_bapertarum
+        }]);
 
       if (error) throw error;
 
@@ -47,7 +115,7 @@ export default async function handler(req, res) {
     }
   } else if (method === 'PUT') {
     try {
-      const { peg_nama, peg_nip, peg_gelar_depan } = req.body;
+      const { peg_nama, peg_nip, peg_gelar_depan, satuan_kerja_id, unit_kerja_id, peg_nip_lama, peg_gelar_belakang, peg_lahir_tempat, peg_lahir_tanggal, peg_foto, peg_jenis_kelamin, peg_status_perkawinan, id_agama, peg_jenis_asn, peg_jenis_pns, status_tkd, peg_status_kepegawaian, peg_cpns_tmt, peg_pns_tmt, id_pend_awal, peg_pend_awal_th, id_pend_akhir, peg_pend_akhir_th, jabatan_id, peg_status_gaji, kedudukan_pegawai, gol_id_awal, peg_gol_awal_tmt, gol_id_akhir, peg_gol_akhir_tmt, peg_karpeg, peg_no_askes, peg_karsutri, peg_bapertarum } = req.body;
 
       if (!peg_nama && !peg_nip && !peg_gelar_depan) {
         return res.status(400).json({ message: 'Tidak ada kolom untuk diperbarui' });
@@ -56,7 +124,40 @@ export default async function handler(req, res) {
       const { data, error } = await supabase
       .schema('siap_skpd')
         .from('spg_pegawai')
-        .update({ peg_nama, peg_nip, peg_gelar_depan })
+        .update({ satuan_kerja_id,
+          unit_kerja_id,
+          peg_nip,
+          peg_nip_lama,
+          peg_nama,
+          peg_gelar_depan,
+          peg_gelar_belakang,
+          peg_lahir_tempat,
+          peg_lahir_tanggal,
+          peg_foto,
+          peg_jenis_kelamin,
+          peg_status_perkawinan,
+          id_agama,
+          peg_jenis_asn,
+          peg_jenis_pns,
+          status_tkd,
+          peg_status_kepegawaian,
+          peg_cpns_tmt,
+          peg_pns_tmt,
+          id_pend_awal,
+          peg_pend_awal_th,
+          id_pend_akhir,
+          peg_pend_akhir_th,
+          jabatan_id,
+          peg_status_gaji,
+          kedudukan_pegawai,
+          gol_id_awal,
+          peg_gol_awal_tmt,
+          gol_id_akhir,
+          peg_gol_akhir_tmt,
+          peg_karpeg,
+          peg_no_askes,
+          peg_karsutri,
+          peg_bapertarum })
         .eq('peg_id', id);
 
       if (error) throw error;
