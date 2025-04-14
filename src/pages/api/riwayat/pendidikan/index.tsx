cabd1c46e5bda96be11fd0e7ce481aadfee2f1ba
Rezky Yayang Yakhamid <60925883+rezkyyayang@users.noreply.github.com>
2025-04-14 14:01:26 +0700

    Update index.tsx

--- src/pages/api/riwayat/pendidikan/index.tsx
+++ src/pages/api/riwayat/pendidikan/index.tsx
@@ -1,4 +1,6 @@
-import { prisma } from '../../../../../lib/prisma'; // Sesuaikan path import
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
