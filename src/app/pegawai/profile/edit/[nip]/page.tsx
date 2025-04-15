95044f7c0ef5ed7c7832e5ec6beb054cc53ffb7a
pujisubarkah <67720528+pujisubarkah@users.noreply.github.com>
2025-04-15 18:23:28 +0700

    Update page.tsx

--- src/app/pegawai/profile/edit/[nip]/page.tsx
+++ src/app/pegawai/profile/edit/[nip]/page.tsx
@@ -1,14 +1,14 @@
-
 "use client";
+
 import { useEffect, useState } from "react";
 import Navbar from "../navbar";
-
+import Profile from "../navbar_profile";
+import RootLayout from "../layout";
 import Datapribadi from "../data-pribadi";
 import FilePegawai from "../file-pegawai";
 import Fungsional from "../fungsional";
 import Struktural from "../struktural";
 import Jabatan from "../jabatan";
-import Profile from "../navbar_profile";
 import Teknis from "../teknis";
 import Klasikal from "../klasikal";
 import NonKlasikal from "../nonklasikal";
@@ -22,35 +22,31 @@ import Prestasi from "../prestasi";
 import Assessmen from "../assessmen";
 import Kepangkatan from "../pangkat";
 import Pendidikan from "../pendidikan";
+import Kedudukan from "../kedudukan";
+import Kesehatan from "../kesehatan";
+import Hukuman from "../hukuman";
 import Penghargaan from "../penghargaan";
 import Pasangan from "../pasangan";
 import OrangTua from "../orangtua";
 import Anak from "../anak";
 import Saudara from "../saudara";
 import Kontak from "../kontak";
-import RootLayout from "../layout";
 
 const Page = () => {
-  // Ambil NIP dari URL
   const nip = window.location.pathname.split("/").pop();
-
-  // State untuk tab yang aktif
   const [activeTab, setActiveTab] = useState<string>(window.location.hash || "#data-pribadi");
 
-  // Fungsi untuk mengubah tab yang aktif
   const handleTabChange = (tab: string) => {
     setActiveTab(tab);
-    window.location.hash = tab; // Perbarui hash di URL
+    window.location.hash = tab;
   };
 
-  // Gunakan useEffect untuk mendeteksi perubahan hash URL
   useEffect(() => {
     const handleHashChange = () => {
       setActiveTab(window.location.hash || "#data-pribadi");
     };
 
     window.addEventListener("hashchange", handleHashChange);
-
     return () => {
       window.removeEventListener("hashchange", handleHashChange);
     };
@@ -64,137 +60,73 @@ const Page = () => {
     );
   }
 
+  const renderTabContent = () => {
+    switch (activeTab) {
+      case "#data-pribadi":
+        return <Datapribadi params={{ nip }} />;
+      case "#file-pegawai":
+        return <FilePegawai />;
+      case "#pendidikan":
+        return <Pendidikan nip={nip} />;
+      case "#pelatihan-fungsional":
+        return <Fungsional />;
+      case "#pelatihan-struktural":
+        return <Struktural />;
+      case "#pelatihan-teknis":
+        return <Teknis />;
+      case "#pelatihan-klasikal":
+        return <Klasikal />;
+      case "#pelatihan-Non-klasikal":
+        return <NonKlasikal />;
+      case "#sertifikasi":
+        return <Sertifikasi />;
+      case "#jabatan":
+        return <Jabatan />;
+      case "#pltplh":
+        return <Pltplh />;
+      case "#kepangkatan":
+        return <Kepangkatan />;
+      case "#kgb":
+        return <Kgb />;
+      case "#cuti":
+        return <Cuti />;
+      case "#kedudukan":
+        return <Kedudukan />;
+      case "#kesehatan":
+        return <Kesehatan />;
+      case "#hukuman":
+        return <Hukuman />;
+      case "#penghargaan":
+        return <Penghargaan />;
+      case "#publikasi":
+        return <Publikasi />;
+      case "#timkerja":
+        return <Timkerja />;
+      case "#prestasi":
+        return <Prestasi />;
+      case "#assessmen":
+        return <Assessmen />;
+      case "#pasangan":
+        return <Pasangan />;
+      case "#anak":
+        return <Anak />;
+      case "#orangtua":
+        return <OrangTua />;
+      case "#saudara":
+        return <Saudara />;
+      case "#kontak":
+        return <Kontak />;
+      default:
+        return <div>Tab tidak ditemukan.</div>;
+    }
+  };
+
   return (
     <div className="space-y-4">
       <Navbar />
       <Profile nip={nip} />
-     
-
       <main className="container mx-auto p-2 space-y-4">
-        {activeTab === "#data-pribadi" && (
-          <section id="data-pribadi">
-            <Datapribadi params={{ nip }} />
-          </section>
-        )}
-        {activeTab === "#file-pegawai" && (
-          <section id="file-pegawai">
-            <FilePegawai />
-          </section>
-        )}
-        {activeTab === "#pendidikan" && (
-          <section id="pendidikan">
-            <Pendidikan nip={nip} />
-          </section>
-        )}
-        {activeTab === "#pelatihan-fungsional" && (
-          <section id="pelatihan-fungsional">
-            <Fungsional />
-          </section>
-        )}
-        {activeTab === "#pelatihan-struktural" && (
-          <section id="pelatihan-struktural">
-            <Struktural />
-          </section>
-        )}
-        {activeTab === "#pelatihan-teknis" && (
-          <section id="pelatihan-teknis">
-            <Teknis />
-          </section>
-        )}
-        {activeTab === "#pelatihan-klasikal" && (
-          <section id="pelatihan-klasikal">
-            <Klasikal />
-          </section>
-        )}
-
-        {activeTab === "#pelatihan-Non-klasikal" && (
-          <section id="pelatihan-Non-klasikal">
-            <NonKlasikal />
-          </section>
-        )}
-        {activeTab === "#sertifikasi" && (
-          <section id="sertifikat">
-            <Sertifikasi />
-          </section>
-        )}
-
-        {activeTab === "#jabatan" && (
-          <section id="jabatan">
-            <Jabatan />
-          </section>
-        )}
-
-        {activeTab === "#pltplh" && (
-          <section id="pltplh">
-            <Pltplh />
-          </section>
-        )}
-        {activeTab === "#kepangkatan" && (
-          <section id="kepangkatan">
-            <Kepangkatan />
-          </section>
-        )}
-        {activeTab === "#kgb" && (
-        <section id="kgb">
-        <Kgb />
-        </section>
-        )}
-        {activeTab === "#cuti" && (
-        <section id="cuti">
-        <Cuti />
-        </section>
-
-        )}
-        {activeTab === "#penghargaan" && (
-          <section id="penghargaan">
-            <Penghargaan />
-          </section>
-        )}
-        {activeTab === "#publikasi" && (
-        <section id="publikasi">
-        <Publikasi />
-        </section>
-        )}
-        {activeTab === "#timkerja" && (
-        <section id="timkerja">
-        <Timkerja />
-        </section>
-        )}
-        {activeTab === "#prestasi" && (
-        <section id="prestasi">
-        <Prestasi />
-        </section>
-        )}
-        {activeTab === "#assessmen" && (
-          <section id="assessmen">
-            <Assessmen />
-          </section>
-        )}
-        {activeTab === "#pasangan" && (
-          <section id="pasangan">
-            <Pasangan />
-          </section>
-        )}
-        {activeTab === "#anak" && (
-          <section id="anak">
-            <Anak />
-          </section>
-        )}
-        {activeTab === "#orangtua" && (
-          <section id="orangtua">
-            <OrangTua />
-          </section>
-        )}
-        {activeTab === "#saudara" && (
-          <section id="saudara">
-            <Saudara />
-          </section>
-        )}
-        {activeTab === "#kontak" && (
-          <section id="kontak">
-            <Kontak />
-          </section>
-        )}
+        <section id={activeTab.replace("#", "")}>{renderTabContent()}</section>
       </main>
     </div>
   );
